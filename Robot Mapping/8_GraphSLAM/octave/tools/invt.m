829c2eb28bfa587dc9093f88fbd9be5342a46888
jean <j.z.feng@foxmail.com>
2018-05-06 08:37:46 +0800

    delete

--- Robot Mapping/8_GraphSLAM/octave/tools/invt.m
+++
@@ -1,5 +0,0 @@
-% inverts a homogenous transform
-function A = invt(m)
-  A = [m(1:2, 1:2)' [0 0]'; [0 0 1]];
-  A(1:2, 3) = -A(1:2, 1:2) * m(1:2, 3);
-end
--- Robot Mapping/8_GraphSLAM/octave/tools/normalize_angle.m
+++
@@ -1,7 +0,0 @@
-function [phi] = normalize_angle(phi)
-% Normalize phi to be between -pi and pi
-% phi can also be a vector of angles
-
-phi = mod(phi + pi, 2*pi) - pi;
-
-end
