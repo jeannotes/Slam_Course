1a85d4fb574b12e8b1ca2fd7ea1524e4a8b5483f
jean <j.z.feng@foxmail.com>
2018-05-27 12:49:50 +0800

    demo4

---
+++ cmake-demo/Demo4/build/CMakeFiles/demo4.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo4.dir/main.cc.o"
+  "demo4.pdb"
+  "demo4"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo4.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
