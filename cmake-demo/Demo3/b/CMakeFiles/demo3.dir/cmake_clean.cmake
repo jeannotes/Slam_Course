c5b24a807d53508d13fd0b3fe883369dc9c10edd
jean <j.z.feng@foxmail.com>
2018-05-27 11:06:00 +0800

    add cmake learning notes

---
+++ cmake-demo/Demo3/b/CMakeFiles/demo3.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/demo3.dir/main.cc.o"
+  "demo3.pdb"
+  "demo3"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/demo3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
