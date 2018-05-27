ee6024e309d14315d3096d6f19a6350ac3caa489
jean <j.z.feng@foxmail.com>
2018-05-27 12:49:34 +0800

    demo3

---
+++ cmake-demo/Demo3/build/CMakeFiles/demo3.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/jean/Documents/code/cmake-demo/Demo3/main.cc" "/home/jean/Documents/code/cmake-demo/Demo3/b/CMakeFiles/demo3.dir/main.cc.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  "/home/jean/Documents/code/cmake-demo/Demo3/b/math/CMakeFiles/MathFunctions.dir/DependInfo.cmake"
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
