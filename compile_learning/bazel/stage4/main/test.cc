b7ccce31962a8254ee7e469a54da611530804908
jean <j.z.feng@foxmail.com>
2018-06-15 19:28:56 +0800

    add stage4 for local repository

---
+++ compile_learning/bazel/stage4/main/test.cc
@@ -0,0 +1,7 @@
+#include <gtest/gtest.h>
+#include "s/include/dd/hello-greet.h"
+
+TEST(HelloTest, GetGreet) {
+  //EXPECT_EQ(std::cout("Bazel"), "Hello Bazel");
+  std::cout << "Bazel" <<std::endl;
+}
