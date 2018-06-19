a04493690a0aa576e6d31ffe7e7f2bbed3e15910
jean <j.z.feng@foxmail.com>
2018-06-19 14:58:14 +0800

    add notes

--- compile_learning/bazel/stage4/main/s/include/hello-greet.cc
+++
@@ -1,6 +0,0 @@
-#include "dd/hello-greet.h"
-#include <string>
-
-std::string get_greet(const std::string& who) {
-  return "Hello " + who;
-}
--- compile_learning/bazel/stage4/main/test.cc
+++
@@ -1,7 +0,0 @@
-#include <gtest/gtest.h>
-#include "s/include/dd/hello-greet.h"
-
-TEST(HelloTest, GetGreet) {
-  //EXPECT_EQ(std::cout("Bazel"), "Hello Bazel");
-  std::cout << "Bazel" <<std::endl;
-}
